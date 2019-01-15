735f94a2098536a27d56f0e162c2b4250e779450
KingVulkan <manowfelow@gmail.com>
2019-01-15 21:07:06 +0300

    first commit

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes';
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
