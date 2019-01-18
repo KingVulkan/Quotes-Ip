ef7042cf21d8ac2824a6f450aee1f66f12d2bc6f
KingVulkan <manowfelow@gmail.com>
2019-01-18 11:05:14 +0300

    adds event emmiters

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter , Input , Output } from '@angular/core';
 import {Quotes} from '../quotes' ;
+import { Quote } from '@angular/compiler';
+
 @Component({
   selector: 'app-quote-details',
   templateUrl: './quote-details.component.html',
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+  @Input()quote: Quotes ;
+  @Output() isComplete = new EventEmitter<boolean>() ;
+
+  quoteComplete(complete: boolean) {
+    this.isComplete.emit(complete) ;
+  }
 
   constructor() { }
 
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -6,6 +6,19 @@ import { Quotes } from '../quotes';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+   quotes = [
+        new Quotes(1, ' Begin now to be what you will be hereafter', 'Saint Jerome'),
+        new Quotes(2, 'You have as much laughter as you have faith', 'Martin Luther' ) ,
+        new Quotes(3, 'The brain is wider than the sky', 'Emily Dickson'),
+        new Quotes(4, 'A goal should scare you a little and excite you a lot', 'Jade Williams'),
+        new Quotes(5, 'Magic is believing in yourself, if you can do that, you can make anything happen', 'Johanne' ),
+        new Quotes(6, 'The world is full of magic things, patiently waiting for our senses to grow sharper. W.B' , 'Abu Vulkan' ),
+
+    ];
+
+    toogleDetails(index) {
+        this.quotes[index].showDescription = !this.quotes[index].showDescription;
+    }
 
   constructor() { }
 
