a3f274a47d6de1058dc312e5c053b39d69e10a59
KingVulkan <manowfelow@gmail.com>
2019-01-18 08:52:19 +0300

    adds details in component

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -6,7 +6,7 @@ import { Quotes } from '../quotes';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
-  newQuote = new Quote(0, '' , '' , '' , '' , new Date());
+  newQuote = new Quotes(0, '' , '');
   @Output() addQuote = new EventEmitter<Quotes>();
 
   submitQuote() {
